5fba1725beb89daaace7e716e469d02fe650a4cb
mitis <mitiskyn@outlook.com>
2023-03-09 15:27:33 +0300

    Сделали Task 2

---
+++ Taks_2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Веведите целое число A");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Веведите целое число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int max =0;
+
+{
+    if(numberA > numberB)
+    {
+        Console.WriteLine($"max={numberA}");
+        Console.WriteLine($"min={numberB}");
+    }
+    else
+    {
+        Console.WriteLine($"max={numberB}");
+        Console.WriteLine($"min={numberA}");
+    }
+}
+    
