803c961f0600b37374888b2f01da83de87b105c9
mitis <mitiskyn@outlook.com>
2023-03-10 11:06:07 +0300

    Сделали Task_9 через функцию

--- Task_9/Program.cs
+++ Task_9/Program.cs
@@ -7,12 +7,26 @@
 // 85 -> 8
 
 int number = new Random().Next(10,100);
-Console.WriteLine($"Cлучайное число из отрезка 10 - 99->{number}");
-int firstDigit = number / 10;
-int secondDigit = number % 10;
- if(firstDigit > secondDigit) 
- Console.WriteLine($"Найбольшая цифтра числа ->{firstDigit}");
- else
-  Console.WriteLine($"Найбольшая цифтра числа ->{secondDigit}");
-int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
-Console.WriteLine($"найбольшая цифра числа ->{maxDigit}");
+// Console.WriteLine($"Cлучайное число из отрезка 10 - 99->{number}");
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+//  if(firstDigit > secondDigit) 
+//  Console.WriteLine($"Найбольшая цифтра числа ->{firstDigit}");
+//  else
+//   Console.WriteLine($"Найбольшая цифтра числа ->{secondDigit}");
+
+  //////////////////////////////////////////////////////////////
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine($"найбольшая цифра числа ->{maxDigit}");
+//////////////////////////////////////////////////////////////
+
+int result = MaxDigit(number) ;
+Console.WriteLine($"найбольшая цифра числа ->{result}");
+
+int MaxDigit(int num) // num = 78
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+    return maxDigit;
+}  
