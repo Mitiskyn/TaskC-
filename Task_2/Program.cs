c4ffba8e20a0f8606aa9cef96fb63b738560f1ea
mitis <mitiskyn@outlook.com>
2023-03-10 12:31:14 +0300

    Поменяли название Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Веведите целое число A");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Веведите целое число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+{
+    if(numberA > numberB)
+    {
+        Console.WriteLine($"max={numberA}");
+        Console.WriteLine($"min={numberB}");
+    }
+    else
+    {
+        Console.WriteLine($"max={numberB}");
+        Console.WriteLine($"min={numberA}");
+    }
+}
+    
