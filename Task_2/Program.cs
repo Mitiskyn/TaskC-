8d1e01e3a64905c6e7b11eb4bb4c37c17689ac73
mitis <mitiskyn@outlook.com>
2023-03-12 16:18:07 +0300

    Отредактировали Task_2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,6 +1,11 @@
-﻿Console.Write("Веведите целое число A");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Веведите число A");
 int numberA = Convert.ToInt32(Console.ReadLine());
-Console.Write("Веведите целое число B");
+Console.Write("Веведите число B");
 int numberB = Convert.ToInt32(Console.ReadLine());
 
 {
