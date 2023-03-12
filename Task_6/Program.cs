d98894fb0762d10e87dce141377778b4e84af9ff
mitis <mitiskyn@outlook.com>
2023-03-12 16:15:32 +0300

    Исправили переменные в Task_6

--- Task_6/Program.cs
+++ Task_6/Program.cs
@@ -4,14 +4,14 @@
 // -3 -> нет
 // 7 -> нет
 
-Console.Write("Веведите целое число A");
-int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Веведите число A" );
+int number = Convert.ToInt32(Console.ReadLine());
 
-if(numberA%2 == 0)
+if(number%2 == 0)
 {
-    Console.WriteLine($"Является четным числом{numberA}");
+    Console.WriteLine($"Является четным числом {number}");
 }
 else
 {
-    Console.WriteLine($"Является не четным числом{numberA}");
+    Console.WriteLine($"Является не четным числом {number}");
 }
