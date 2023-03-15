1493d4dc13bc9e40a5ec9143e4cd58696b5de72b
mitis <mitiskyn@outlook.com>
2023-03-15 23:29:08 +0300

    Доработал Task_19

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -8,12 +8,18 @@ Console.WriteLine("Введите пятизначное число");
 int number = Convert.ToInt32(Console.ReadLine());
 
 if (number < 0) number = number * -1;
-bool palindrome = Palindrome(number);
-
-Console.WriteLine();
-Console.WriteLine(palindrome ? "Да" : "Нет");
-
-bool Palindrome(int num)
+if (number < 10000 || number > 100000)
+{
+Console.WriteLine("Не является пятизначным числом");
+}
+else
+{  bool palindrome = Palindrome(number);
+    Console.WriteLine();
+    Console.WriteLine(palindrome ? "Да" : "Нет");
+}
+    
+    
+    bool Palindrome(int num)
 {
     if (num < 10)
         return false;
@@ -26,3 +32,8 @@ bool Palindrome(int num)
     }
     return initNum == revNum;
 }
+   
+
+
+    
+
