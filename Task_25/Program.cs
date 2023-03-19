0ade6f417243d7262ec3c487c2fac87d4de40b26
mitis <mitiskyn@outlook.com>
2023-03-19 23:04:05 +0300

    Сделали Task_25

---
+++ Task_25/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите целое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите целое число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int natureDigit = NatureDigit(numberA, numberB);
+Console.WriteLine($"Число {numberA} в степени {numberB} = {natureDigit}");
+
+
+int NatureDigit(int numA, int numB)
+{
+    int nnum = numA;
+    if(numB==0) nnum = 1;
+
+    for(int i = 1; i < numB; i++)
+    {
+        checked
+        {
+        nnum = nnum * numA;
+        }
+    }
+    return nnum;
+
+}
