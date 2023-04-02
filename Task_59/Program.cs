a5a8423551920f04c8a42f02f18b0b85a17e95d7
mitis <mitiskyn@outlook.com>
2023-04-03 01:46:08 +0300

    Сделали Task_59

---
+++ Task_59/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задача 59: Задайте двумерный массив из целых чисел.
+// Напишите программу, которая удалит строку и столбец, на
+// пересечении которых расположен наименьший элемент
+// массива.
+
+int[,] array2d = CreateMatrix(5, 7, -10, 10);
+PrintMatrix(array2d);
+int[] coordinates = CordinatesMinimumValue(array2d);
+int[,] newArray2d = MatrixWithoutRowColumnMinValue(array2d, coordinates);
+Console.WriteLine();
+PrintMatrix(newArray2d);
+
+
+int[] CordinatesMinimumValue(int[,] matrix)
+{
+    int minRow = 0;
+    int minColumn = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(matrix[i, j] < matrix[minRow, minColumn])
+            {
+                minRow = i;
+                minColumn = 1;
+            }
+        }
+    }
+    return new int[] { minRow, minColumn};
+}
+
+int[,] MatrixWithoutRowColumnMinValue(int[,] matrix, int[] coords)
+{
+    int[,] newMatrix = new int[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1];
+    for (int i = 0, newRow = 0; i < matrix.GetLength(0); i++)
+    {
+        if (i == coords[0]) continue;
+        for (int j = 0, newColumn = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j == coords[1]) continue;
+            newMatrix[newRow, newColumn] = matrix[i, j];
+            newColumn++;
+        }
+        newRow++;
+    }
+    return newMatrix;
+}
+
+int[,] CreateMatrix(int rows, int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];
+
+    Random random = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = random.Next(min, max + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 4} ");
+        }
+
+        Console.Write("|");
+        Console.WriteLine();
+
+    }
+}
